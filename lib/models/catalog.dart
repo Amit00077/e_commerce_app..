75f09a2d96eb566b2e5059a8e036b9dc32d00313
Amit Baliyan <amitbaliyan071@gmail.com>
2022-02-24 03:44:27 +0530

    day9 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,15 +1,15 @@
 // import 'package:flutter/cupertino.dart';
 
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
 
 class CatalogModel {
-  static final items = [
+  static List<Item> items = [
     Item(
         id: 1,
         name: "iPhone 12 pro",
         desc: "Apple iPhone 12th generation",
         price: 999,
-        color: "",
+        color: "#33505a",
         image: "https://m.media-amazon.com/images/I/71MHTD3uL4L.jpg")
   ];
 }
@@ -29,4 +29,21 @@ class Item {
       required this.price,
       required this.color,
       required this.image});
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+        id: map["id"],
+        name: map["name"],
+        desc: map["desc"],
+        price: map["price"],
+        color: map["color"],
+        image: map["image"]);
+  }
+  toMap() => {
+        "id": id,
+        "name": name,
+        "desc": desc,
+        "price": price,
+        "color": color,
+        "image": image
+      };
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,39 @@
-import 'package:flutter/material.dart';
-import 'package:new_app/models/catalog.dart';
-import 'package:new_app/widgets/widgets.dart';
-import 'package:new_app/widgets/itemwidgets.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:new_app/models/catalog.dart';
+import 'package:new_app/widgets/itemwidgets.dart';
+import 'package:new_app/widgets/widgets.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+@override
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogjson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeddata = jsonDecode(catalogjson);
+    var productdata = decodeddata["products"];
+    CatalogModel.items =
+        List.from(productdata).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
+
+    // print(productData);
+  }
+
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
           title: const Text(
@@ -18,10 +43,10 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
-          itemCount: dummyList.length,
+          itemCount: CatalogModel.items.length,
           itemBuilder: (context, index) {
             return ItemWidgets(
-              item: dummyList[index],
+              item: CatalogModel.items[index],
             );
           },
         ),
