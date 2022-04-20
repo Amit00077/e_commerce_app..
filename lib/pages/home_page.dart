2f4f3a92f4f2166d18f037d2687466fed5fd8a3b
Amit Baliyan <amitbaliyan071@gmail.com>
2022-04-21 03:55:05 +0530

    day11 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:new_app/models/catalog.dart';
-import 'package:velocity_x/velocity_x.dart';
 import 'package:new_app/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+import '../widgets/home_widgets/catalog_header.dart';
+import '../widgets/home_widgets/catalog_list.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -22,25 +23,21 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
-    final catalogjson =
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
         await rootBundle.loadString("assets/files/catalog.json");
-    final decodeddata = jsonDecode(catalogjson);
-    var productdata = decodeddata["products"];
-    CatalogModel.items =
-        List.from(productdata).map<Item>((item) => Item.fromMap(item)).toList();
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
     setState(() {});
-
-    // print(productData);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //     title: const Text(
-      //   "Catalog App",
-      //   style: TextStyle(color: Colors.black),
-      // )),
+      backgroundColor: Mytheme.creamcolor,
       body: SafeArea(
         child: Container(
           padding: Vx.m32,
@@ -49,57 +46,13 @@ class _HomePageState extends State<HomePage> {
             children: [
               const Catalogheader(),
               if (CatalogModel.items.isNotEmpty)
-                const CatalogList().expand()
+                const CatalogList().py16().expand()
               else
-                const Center(
-                  child: CircularProgressIndicator(),
-                )
+                const CircularProgressIndicator().centered().expand().py16(),
             ],
           ),
         ),
       ),
     );
-  }
-}
-
-class Catalogheader extends StatelessWidget {
-  const Catalogheader({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        "Catalog App".text.xl5.bold.color(Mytheme.bluish).make(),
-        "Trending products".text.xl2.make()
-      ],
-    );
-  }
-}
-
-class CatalogList extends StatelessWidget {
-  const CatalogList({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: CatalogModel.items.length,
-      itemBuilder: (context, index) {
-        final catalog = CatalogModel.items[index];
-        return CatalogItem(catalog: catalog);
-      },
-    );
-  }
-}
-
-class CatalogItem extends StatelessWidget {
-  final Item catalog;
-
-  const CatalogItem({Key? key, required this.catalog}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return VxBox().white.square(100).make();
   }
 }
