e96c436ecd17e49c1c41818eaab8a35d890eae27
Amit Baliyan <amitbaliyan071@gmail.com>
2022-04-23 05:28:51 +0530

    day15 compledes

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:new_app/main.dart';
 import 'package:new_app/models/cart.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -16,7 +15,7 @@ class CartPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          const CartList().p24().expand(),
+          CartList().p24().expand(),
           const Divider(),
           CartTotal(),
         ],
@@ -49,25 +48,25 @@ class CartTotal extends StatelessWidget {
   }
 }
 
-class CartList extends StatefulWidget {
-  const CartList({Key? key}) : super(key: key);
-
-  @override
-  State<CartList> createState() => _CartListState();
-}
-
-class _CartListState extends State<CartList> {
+class CartList extends StatelessWidget {
   final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: _cart.items.length,
-      itemBuilder: (context, index) => ListTile(
-        leading: const Icon(Icons.done),
-        trailing: IconButton(
-            onPressed: () {}, icon: const Icon(Icons.remove_circle_outline)),
-        title: _cart.items[index].name.text.make(),
-      ),
-    );
+    return _cart.items.isEmpty
+        ? "Nothing to show ".text.xl3.bold.makeCentered()
+        : ListView.builder(
+            itemCount: _cart.items.length,
+            itemBuilder: (context, index) => ListTile(
+              leading: const Icon(Icons.done),
+              trailing: IconButton(
+                icon: const Icon(Icons.remove_circle_outline),
+                onPressed: () {
+                  _cart.remove(_cart.items[index]);
+                  // setState(() {});
+                },
+              ),
+              title: _cart.items[index].name.text.make(),
+            ),
+          );
   }
 }
